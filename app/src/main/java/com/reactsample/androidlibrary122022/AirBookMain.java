d83fc535f16ba2b0059bed952c4a71808a964555
varshaj123 <74649453+varshaj123@users.noreply.github.com>
2022-12-15 14:24:45 +0530

    Add files via upload
    Project files added

---
+++ app/src/main/java/com/reactsample/androidlibrary122022/AirBookMain.java
@@ -0,0 +1,14 @@
+package com.reactsample.androidlibrary122022;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AirBookMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
